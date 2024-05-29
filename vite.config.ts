adccf82830911c2ae8bed3d1631a2c0587afd4dc
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-05-30 00:06:35 +0530

    changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import path from "path"
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
